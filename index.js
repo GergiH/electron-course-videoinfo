9c6abc2379b7c6f89aedf4d76e2a9287f0a49de6
Horvath Gergely <gergelyzsolt.hrv@gmail.com>
2017-08-15 11:15:35 +0200

    Get video length and showing it in the main window

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron; // same as if called electron.app to app and electron.BrowserWindow
+const { app, BrowserWindow, ipcMain } = electron; // same as if called electron.app to app and electron.BrowserWindow
+
+let mainWindow;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
